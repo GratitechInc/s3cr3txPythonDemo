abed432040e1d57c59069133fb029492f4911780
Gratitech <123098943+GratitechInc@users.noreply.github.com>
2023-02-02 17:58:54 -0600

    Create s3cr3txPythonTest.py

---
+++ s3cr3txPythonTest.py
@@ -0,0 +1,36 @@
+import requests
+import logging
+
+def print_msg(strMessage):
+    logging.debug(strMessage)
+    print(strMessage)
+    
+def main():
+    try:
+        logging.debug('Start of Demonstrating s3cr3tx in python.')
+        DOC_FORMAT="text/plain"
+        Email_Header="you@example.com"
+        API_Token_Header="YourS3cr3txAPIToken"
+        Auth_Code_Header="YourS3cr3txAuthCode"
+        EorD_Header="e"
+        Input_Header="This is a secret!"
+        URL_ROOT = "https://s3cr3tx.com/Values"
+        result3 = requests.get(URL_ROOT,headers={"Accept": DOC_FORMAT ,"Email": Email_Header,"APIToken":API_Token_Header,"AuthCode":Auth_Code_Header,"EorD":EorD_Header,"Input":Input_Header })
+        Output=result3.text
+        print_msg("Your Encrypted text Output is: " + Output)
+        EorD_Header="D"
+        Input_Header=Output
+        result4 = requests.get(URL_ROOT,headers={"Accept": DOC_FORMAT ,"Email": Email_Header,"APIToken":API_Token_Header,"AuthCode":Auth_Code_Header,"EorD":EorD_Header,"Input":Input_Header })
+        Output=result4.text
+        print_msg("Your Decrypted text Output is: " + Output)
+    except Exception as err:
+        print('An error occured: ' + str(err))
+def elementExists(anything):
+    if anything != None:
+        return True
+    else:
+        return False 
+
+
+if __name__ == "__main__":
+    main()
